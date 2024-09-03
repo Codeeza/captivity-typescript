dd040428fa0afe0b0679c5aa5006280d301587c7
Tyler Van Wyk <tylervanwyk01@outlook.com>
2024-09-03 09:48:18 +0200

    new file:   app/international-tolerances/page.tsx 	modified:   app/international-tolerances/page.tsx

---
+++ app/international-tolerances/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import React from "react";
+
+const InternationalTolerances = () => {
+  return (
+    <div className="border-b border-gray-300 shadow-sm font-bold text-gray-500">
+    <div>
+      <div className="mx-auto p-10">
+        <h1 className="text-center text-4xl text-blacktop-10">
+          INTERNATIONAL <span className="font-extrabold text-black">TOLERANCES</span>
+        </h1>
+        <h1 className="text-center text-4xl text-red-600 font-extrabold top-10">
+          FOR APPAREL
+        </h1>
+        <div>
+          <h3 className="text-xl text-red-600 font-bold text-left mt-5">
+            Garment Specification Sheet
+          </h3>
+          <p className="text-gray-500 mt-3">
+            The person, firm or company buying is hereafter termed “The Buyer.
+          </p>
+
+          <h3 className="text-xl text-red-600 font-bold text-left mt-5">
+            COMPANY POLICY
+          </h3>
+          <p className="text-gray-500 mt-3">
+            Our BRAND is strictly distributed via authorised distributors
+            namely: promotional companies, advertising and event companies as
+            well as independent resellers only.
+          </p>
+
+          <div className="p-8">
+
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default InternationalTolerances;
